14648109fa080e87af99eaf0fa22e2b3b69db19a
rahiel <rahielkasim@gmail.com>
2014-11-29 15:37:07 +0100

    prepare for pypi

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ from setuptools import setup
 setup(
     name="shellstats",
     version="0.1",
+    description="Show the most used shell commands.",
+    long_description="blabla",
+    url="https://github.com/rahiel/shellstats",
+    download_url = "https://github.com/rahiel/shellstats/tarball/0.1"
+    author="Rahiel Kasim",
+    author_email="rahielkasim@gmail.com",
+    license="MIT",
     py_modules=["shellstats"],
     install_requires=["click"],
     entry_points={
@@ -11,5 +18,15 @@ setup(
     },
     extras_require = {
         "Plotting": ["matplotlib"]
-    }
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Topic :: System :: Shells",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
 )
